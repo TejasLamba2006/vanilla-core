03878624daa7d9b59abbd6e60f17f7ec404b356f
TejasLamba2006 <tejas22feb@gmail.com>
2025-12-28 11:24:04 +0530

    feat: Update logo source from SVG to PNG and add new logo image

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
                 title: 'SMP Core',
                 logo: {
                     alt: 'SMP Core Logo',
-                    src: 'img/logo.svg',
+                    src: 'img/logo.png',
                 },
                 items: [
                     {
